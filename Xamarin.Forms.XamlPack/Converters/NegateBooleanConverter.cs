793c99b486ad572bb57ac16681a3895df71118d7
Sivamuthu Kumar <ksivamuthu@ceiamerica.com>
2020-01-02 20:25:15 +0530

    Initializing the project

---
+++ Xamarin.Forms.XamlPack/Converters/NegateBooleanConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Xamarin.Forms.XamlPack.Converters
+{
+    public class NegateBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool) return !(bool)value;
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
