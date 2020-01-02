983a4c5a52b0be8cfa74691bf1f09851972d43fe
Sivamuthu Kumar <ksivamuthu@ceiamerica.com>
2020-01-02 21:01:18 +0530

    Add Enum description converter

---
+++ Xamarin.Forms.XamlPack/Converters/EnumDescriptionConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Xamarin.Forms.XamlPack.Converters
+{
+    public class EnumDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumInput = (Enum)value; var type = enumInput.GetType();
+            var memberInfo = type.GetMember(enumInput.ToString());
+
+            if (memberInfo != null && memberInfo.Length > 0)
+            {
+                var attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    return ((DescriptionAttribute)attrs[0]).Description;
+                }
+            }
+
+            return enumInput.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
